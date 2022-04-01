cc11d05fb64cde09ed1035763d66bf8e11daf26c
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-01 10:03:59 +0200

    - [Cards] - Change Effect in Optional<Effect>

--- src/main/java/cards/Card.java
+++ src/main/java/cards/Card.java
@@ -1,12 +1,14 @@
 package cards;
 
+import java.util.Optional;
+
 public interface Card {
     String getIdCard();
     String getName();
     int getLifePoint();
     int getAttack();
     int gatMana();
-    Effect getEffect();
+    Optional<Effect> getEffect();
     int getPlacementRounds();
     void setAttack(int newValue);
     void setLifePoint(int newValue);
