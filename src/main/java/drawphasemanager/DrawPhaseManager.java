247e1f41605b2ce363fdcea92d8b2ac67036e7c0
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-04-01 19:16:58 +0200

    - [ DrawPhase ] - Added a constructor in DrawPhaseManagerImpl and modified implementation on method

--- src/main/java/drawphasemanager/DrawPhaseManager.java
+++ src/main/java/drawphasemanager/DrawPhaseManager.java
@@ -4,19 +4,8 @@ import java.util.List;
 
 import cards.Card;
 import shared.PhaseManager;
-import shared.Player;
 
 public interface DrawPhaseManager extends PhaseManager {
-	
-    /**
-     *     In futuro aggiungeremo il parametro per passare la BoardGUI.
-     *     when called you can manage hands of
-     *     each Player using the boardGUI.
-     * 
-     * @param player is the player that interact with GUI
-     * @param playerIA is an IA that makes auto moves
-     */
-    void drawPhaseManager(Player player, Player playerIA);
 	
     /**
      *     when called set mana and update the GUI.
--- src/main/java/drawphasemanager/DrawPhaseManagerImpl.java
+++ src/main/java/drawphasemanager/DrawPhaseManagerImpl.java
@@ -2,7 +2,6 @@ package drawphasemanager;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import cards.Card;
 import shared.Player;
@@ -11,10 +10,15 @@ public class DrawPhaseManagerImpl implements DrawPhaseManager {
     
     private static final boolean PLAYER_TURN = true;
     private static final int NO_MORE_CARDS = 0;
-    private Player player;
-    private Player playerIA;
+    private final Player player;
+    private final Player playerIA;
     private List<Card> currentDeck;
     private List<Card> currentHand;
+    
+    public DrawPhaseManagerImpl (final Player player, final Player playerIA) {
+        this.player = player;
+        this.playerIA = playerIA;
+    }
 
     /**
      * {@inheritDoc}
@@ -23,15 +27,6 @@ public class DrawPhaseManagerImpl implements DrawPhaseManager {
     public void handleEffect() {
         // TODO Auto-generated method stub
 
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void drawPhaseManager(final Player player, final Player playerIA) {
-        this.player = player;
-        this.playerIA = playerIA;
     }
 
     /**
@@ -55,8 +50,8 @@ public class DrawPhaseManagerImpl implements DrawPhaseManager {
     private void manaAndHand(final Player currentPlayer) {
         if (currentPlayer.getDeck().size() > DrawPhaseManagerImpl.NO_MORE_CARDS) {
             currentPlayer.getMana();
-            this.currentDeck = new ArrayList<>(currentPlayer.getDeck().stream().collect(Collectors.toList()));
-            this.currentHand = new ArrayList<>(currentPlayer.getHand().stream().collect(Collectors.toList()));
+            this.currentDeck = currentPlayer.getDeck();
+            this.currentHand = currentPlayer.getHand();
             
             this.currentHand.add(this.currentDeck.get(this.currentDeck.size() - 1));
             this.currentDeck.remove(this.currentDeck.size() - 1);
