41733420d3a0827ab8b56b371316e5767ff17cb1
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-04-03 18:00:47 +0200

    - [ BoardGUI ] - Modified the class to launch application

--- src/main/java/main/application/App.java
+++ src/main/java/main/application/App.java
@@ -5,6 +5,7 @@ package main.application;
 
 
 import javafx.application.Application;
+import view.MainApplication;
 
 /** Main application entry-point. */
 
@@ -12,7 +13,7 @@ public final class App {
     private App() { }
 
     public static void main(final String[] args) {
-        Application.launch(JavaFXAppWithFXML.class, args);
+        Application.launch(MainApplication.class, args);
         // The following line raises: Error: class it.unibo.samplejavafx.App is not a subclass of javafx.application.Application
         //JavaFXApp.launch(args);
         // While the following would do just fine:
