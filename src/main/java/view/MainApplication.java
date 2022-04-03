518318af8e9dfbb4e4963c3a60a47479829acd25
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-04-03 14:08:21 +0200

    - [ BoardGUI ] - Create a new class where start the application

---
+++ src/main/java/view/MainApplication.java
@@ -0,0 +1,30 @@
+package view;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX entry point, it launches the main menu
+ */
+public class MainApplication extends Application {
+
+    /**
+     * Initializes menu theme and sets all graphics components of main menu
+     */
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        
+        final Showable menuGUI = new MenuGUI(primaryStage);
+        
+        primaryStage.setScene(menuGUI.getScene());
+        primaryStage.getScene().getStylesheets().add("application.css");
+        primaryStage.setOnCloseRequest((e) -> {
+            Platform.exit();
+        });
+        
+        primaryStage.show();
+        
+    }
+
+}
