6bf384161b123be6e2260f0641ffe32d12cd2190
Gianluca Consoli <gianluca.consoli@studio.unibo.it>
2022-04-03 17:59:18 +0200

    - [ BoardGUI ] - Added to MenuGUI: style for buttons and a background

--- src/main/java/view/MenuGUI.java
+++ src/main/java/view/MenuGUI.java
@@ -1,14 +1,19 @@
 package view;
 
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import static view.ScreenDimension.WIDTH;
 import static view.ScreenDimension.HEIGHT;
 
 public class MenuGUI implements Showable {
+    
+    private static final String STYLE_MENU_BUTTON = "buttonMenu";
     
     private final Stage stage;
     private Scene scene;
@@ -25,13 +30,28 @@ public class MenuGUI implements Showable {
         final BorderPane layout = new BorderPane();
         this.scene = new Scene(layout, WIDTH.getValue(), HEIGHT.getValue());
         
-        final Button centerButton = new Button("CENTER BUTTON");
-        centerButton.setMinWidth(150);
-        
         layout.setTop(null);
-        layout.setCenter(centerButton);
+        layout.setCenter(this.centerNode());
         layout.setLeft(null);
         layout.setRight(null);
+        
+        this.scene.getStylesheets().add("application.css");
+    }
+    
+    private Node centerNode() {
+        final VBox centralPanel = new VBox();
+        centralPanel.setAlignment(Pos.CENTER);
+        centralPanel.setSpacing(40);
+        
+        final Button startB = new Button("START GAME");
+        final Button quitB = new Button("QUIT");
+        
+        startB.getStyleClass().add(MenuGUI.STYLE_MENU_BUTTON);
+        quitB.getStyleClass().add(MenuGUI.STYLE_MENU_BUTTON);
+        
+        centralPanel.getChildren().addAll(startB, quitB);
+        
+        return centralPanel;
     }
     
     /**
