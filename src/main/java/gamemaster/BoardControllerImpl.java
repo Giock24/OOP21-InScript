518c222bc3680c95a4d182dc181b159f7977ce1d
Riccardo Gardenghi <riccardo.gardenghi@studio.unibo.it>
2022-04-03 21:14:50 +0200

    - [ BoardGUI ] - inizialization of some fuction for generate the cards

--- src/main/java/gamemaster/BoardControllerImpl.java
+++ src/main/java/gamemaster/BoardControllerImpl.java
@@ -1,71 +1,99 @@
 package gamemaster;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import cards.Card;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
 
 public class BoardControllerImpl {
     
     GameMaster gameMaster;
+    Optional<Integer> selectedCardToPlace;
     
-    @FXML private Button firstPlayerBoardCell;
+    
+    ////player info/////
+    @FXML private Label lifePointsPlayer;
+    @FXML private Label currentManaPlayer;
+    @FXML private Label manaPlayer;
+    
+    ////player IA info////
+    @FXML private Label lifePointsIA;
+    @FXML private Label currentManaIA;
+    @FXML private Label manaIA;
+    
+    
+    ////cards//////
+    @FXML private HBox boardIA;
+    @FXML private HBox boardPlayer;
+    @FXML private ScrollPane handPlayer;
+    
     
     public void initialize(){
         //TODO: add the deck here
         gameMaster= new GameMasterImpl(new ArrayList<Card>(), new ArrayList<Card>());
-    }
-
-    @FXML
-    private void onFirstHandCellPress(Event event) {
-        // TODO Auto-generated method stub
-    }
-    @FXML
-    private void onSecondHandCellPress(Event event) {
-        // TODO Auto-generated method stub
-    }
-    @FXML
-    private void onThirdHandCellPress(Event event) {
-        // TODO Auto-generated method stub
-    }
-    @FXML
-    private void onForthHandCellPress(Event event) {
-        // TODO Auto-generated method stub
-    }
-    @FXML
-    private void onFifthHandCellPress(Event event) {
-        // TODO Auto-generated method stub
+       
+        //TODO: generate the elements for visualize the cards cards in the board for the IA
+        
+        //TODO: generate the elements for visualize the cards in the board for the player
+        //TODO: generate the elements for visualize the cards in the hand of the player (scrollPane)
+        
     }
     
-
-    @FXML
-    public void onFirstBoardCellPress(Event event) {
-        // TODO Auto-generated method stub
+    private VBox generateEmptyCardCell() {
+        final VBox emptyCardCell = new VBox();
         
-    };
-    @FXML
-    public void onSecondBoardCellPress(Event event) {
-        // TODO Auto-generated method stub
+        return emptyCardCell;
+    }
+    
+    private VBox generateCardElement(Card card) {
         
-    };
-    @FXML
-    public void onThirdBoardCellPress(Event event) {
-        // TODO Auto-generated method stub
+        final VBox cardElement = new VBox();
+        cardElement.setAlignment(Pos.CENTER);
+        //centralPanel.setSpacing(40);
         
-    };
-    @FXML
-    public void onForthBoardCellPress(Event event) {
-        // TODO Auto-generated method stub
+        final Label cardName = new Label();
+        cardName.setText(card.getName());
         
-    };
-    @FXML
-    public void onFifthBoardCellPress(Event event) {
-        // TODO Auto-generated method stub
+        //TODO add all the card element
         
-    };
+        return cardElement;
+    }
+    
+    /**
+     * function set on every card on the player hand for select the card to place
+     * 
+     * @param cardPosInThePlayerHand index of the player field hand
+     */
+    private void onCardSelected(int cardPosInThePlayerHand) {
+        selectedCardToPlace=Optional.of(cardPosInThePlayerHand);
+    }
+    
+    /**
+     * this function generate and then
+     */
+    private void updateView() {
+        //TODO: update the elements for visualize the cards in the board for the player
+        //TODO: update the elements for visualize the cards cards in the board for the IA
+        //TODO: update the elements for visualize the cards in the hand of the player (scrollPane)
+    }
+    
+    
+    /**
+     * function connect to the end-turn button
+     * 
+     * @param event
+     */
+    @FXML
+    private void onEndTrunButtonPress(Event event) {
+        // TODO Auto-generated method stub
+    }
 
 
 }
