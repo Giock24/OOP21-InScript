37e581eafb61490452ec237dea84c7eb1bae4790
Riccardo Gardenghi <riccardo.gardenghi@studio.unibo.it>
2022-04-04 19:26:00 +0200

    - [ Shared ] - set the dimension of boards

--- src/main/java/gamemaster/GameMasterImpl.java
+++ src/main/java/gamemaster/GameMasterImpl.java
@@ -18,8 +18,8 @@ public class GameMasterImpl implements GameMaster {
 
     public GameMasterImpl(ArrayList<Card> humanPlayerDeck, ArrayList<Card> aiPlayerDeck, boolean isTheAIturn) {
         super();
-        this.humanPlayer = new PlayerImpl(humanPlayerDeck, GameMasterImpl.DEFAULT_PLAYER_LIFE,  GameMasterImpl.INITIAL_MANA, GameMasterImpl.INITIAL_MANA, new ArrayList<Optional<Card>>(), new ArrayList<Card>());
-        this.aiPlayer = new PlayerImpl(aiPlayerDeck,  GameMasterImpl.DEFAULT_PLAYER_LIFE, GameMasterImpl.INITIAL_MANA, GameMasterImpl.INITIAL_MANA, new ArrayList<Optional<Card>>(), new ArrayList<Card>());
+        this.humanPlayer = new PlayerImpl(humanPlayerDeck, GameMasterImpl.DEFAULT_PLAYER_LIFE,  GameMasterImpl.INITIAL_MANA, GameMasterImpl.INITIAL_MANA, new ArrayList<Optional<Card>>(Player.NUM_CARD_BOARD), new ArrayList<Card>());
+        this.aiPlayer = new PlayerImpl(aiPlayerDeck,  GameMasterImpl.DEFAULT_PLAYER_LIFE, GameMasterImpl.INITIAL_MANA, GameMasterImpl.INITIAL_MANA, new ArrayList<Optional<Card>>(Player.NUM_CARD_BOARD), new ArrayList<Card>());
         this.isTheAIturn = isTheAIturn;
     }
 
