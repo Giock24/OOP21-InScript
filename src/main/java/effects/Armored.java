d31608e7bb71f60c46d2cda63116fc72d9eabc69
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-13 10:16:08 +0200

    - [Cards] - Create and Implement Armored Effect

---
+++ src/main/java/effects/Armored.java
@@ -0,0 +1,27 @@
+package effects;
+
+import cards.AbstractEffect;
+import cards.ActivationEvent;
+import shared.Player;
+
+public class Armored extends AbstractEffect {
+
+    public Armored() {
+        super("Armored", "Questa carta quando subisce danno da combattimento lo dimezza");
+    
+    }
+
+    @Override
+    public ActivationEvent getActivationEvent() {
+        
+        return ActivationEvent.ONDEFENDING;
+    }
+
+    @Override
+    public void useEffect(final Player cardOwner, final Player enemy, final int boardPosition) {
+        
+        cardOwner.getCurrentBoard().get(boardPosition).setLifePoint(cardOwner.getCurrentBoard().get(boardPosition).getLifePoint() + (enemy.getCurrentBoard().get(boardPosition).getAttack() / 2));
+        
+    }
+
+}
