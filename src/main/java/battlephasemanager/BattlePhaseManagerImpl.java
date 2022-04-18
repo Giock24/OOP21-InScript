5951bc41ad5093da7eee87559b687124fff12515
Giovanni Babbi <giovanni.babbi@studio.unibo.it>
2022-04-18 10:26:58 +0200

    - [BattlePhase] - Created implementation for BattlePhaseManager

---
+++ src/main/java/battlephasemanager/BattlePhaseManagerImpl.java
@@ -0,0 +1,17 @@
+package battlephasemanager;
+
+public class BattlePhaseManagerImpl implements BattlePhaseManager {
+
+    @Override
+    public void handleEffect() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean startBattle(boolean isTheAIturn) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
