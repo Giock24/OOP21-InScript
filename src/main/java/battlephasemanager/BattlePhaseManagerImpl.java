12dd65ac6352d6e05220abe41ff823c5e623f89c
Giovanni Babbi <giovanni.babbi@studio.unibo.it>
2022-04-20 09:52:41 +0200

    - [ BattePhase ] - Updated BattlePhaseManagerImpl

--- src/main/java/battlephasemanager/BattlePhaseManagerImpl.java
+++ src/main/java/battlephasemanager/BattlePhaseManagerImpl.java
@@ -7,26 +7,26 @@ import java.util.Optional;
 import cards.ActivationEvent;
 import cards.Card;
 import shared.Player;
-import shared.PlayerImpl;
 
 public class BattlePhaseManagerImpl implements BattlePhaseManager {
     
-    private Player player;
-    private Player enemy;
+    private final Player player;
+    private final Player enemy;
 
     private List<Optional<ActivationEvent>> effectPlayer = new ArrayList<>();
     private List<Optional<ActivationEvent>> effectEnemy = new ArrayList<>();
     private final List<ActivationEvent> eventTarget = List.of(ActivationEvent.ONATTAKING, ActivationEvent.ONDEFENDING, ActivationEvent.ONDEATH);
-    private List<Optional<Card>> postBattleBoard = new ArrayList<>();
+    // private List<Optional<Card>> postBattleBoard = new ArrayList<>();
     
     public BattlePhaseManagerImpl(final Player p1, final Player p2) {
         this.player = p1;
-        this.player = p2;
+        this.enemy = p2;
     }
     
     private List<Optional<ActivationEvent>> extractEvent(final Player target) {
         final List<Optional<ActivationEvent>> tmp = new ArrayList<>();
-        for(Optional<Card> card : target.getCurrentBoard()) {
+        // final aggiunto per sopprimere un warning, possibile problema;
+        for(final Optional<Card> card : target.getCurrentBoard()) {
             if(card.isPresent()) {
                 if(card.get().getEffect().isPresent() && eventTarget.contains(card.get().getEffect().get().getActivationEvent())) {
                     tmp.add(Optional.of(card.get().getEffect().get().getActivationEvent()));
@@ -46,22 +46,44 @@ public class BattlePhaseManagerImpl implements BattlePhaseManager {
         
     }
     
-    private List<Optional<Card>> handleBattle(Player protagonist){
-        for(Optional<Card> card : protagonist.getCurrentBoard()) {
-            if(card.isPresent()) {
-                
+    private List<Optional<Card>> handleBattle(final Player protagonist, final Player antagonist){
+        final List<Optional<Card>> tmp = new ArrayList<>();
+        for(int i = 0; i < protagonist.getCurrentBoard().size(); i++) {
+            if(protagonist.getCurrentBoard().get(i).isPresent()) {
+                if(antagonist.getCurrentBoard().get(i).isPresent()) {
+                    // Metodo dello sviluppo della battaglia.
+                    // da aggiungere la parte per gestire gli effetti
+                    
+                    
+                    
+                    
+                    // Se la carta è presente sottrae alla vita attuale di tale carta l'attacco della carta del protagonista
+                    // antagonist.getCurrentBoard().get(i).get().SetLife(antagonist.getCurrentBoard().get(i).get().getLife() - protagonist.getCurrentBoard().get(i).get().getAttack())
+                    if(antagonist.getCurrentBoard().get(i).get().getLifePoint() <= 0) {
+                        tmp.add(Optional.empty());
+                    }
+                    else {
+                        tmp.add(antagonist.getCurrentBoard().get(i));
+                    }
+                }
+                else {
+                    antagonist.setLifePoints(antagonist.getLifePoints() - protagonist.getCurrentBoard().get(i).get().getAttack());
+                }
             }
         }
-        return postBattleBoard;
+        return tmp;
     }
 
     @Override
-    public boolean startBattle(boolean isTheAIturn) {
+    public boolean startBattle(final boolean isTheAIturn) {
         handleEffect();
         if(isTheAIturn) {
-            
+            player.setCurrentBoard(handleBattle(enemy, player)); 
         }
-        return false;
+        else {
+            enemy.setCurrentBoard(handleBattle(player, enemy)); 
+        }
+        return player.getLifePoints() <= 0 || enemy.getLifePoints() <= 0;
     }
 
 }
