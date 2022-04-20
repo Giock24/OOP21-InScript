acb04a4f0b138c8225fe48e6689de29dcc9aa4f7
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-20 17:28:12 +0200

    - [Shared] - Add setter in Card Interface

--- src/main/java/cards/Card.java
+++ src/main/java/cards/Card.java
@@ -3,11 +3,15 @@ package cards;
 import java.util.Optional;
 
 public interface Card {
-	String getIdCard();
-	String getName();
-	int getLifePoint();
-	int getAttack();
-	int gatMana();
-	Optional<Effect> getEffect();
-	int getPlacementRounds();
+    String getIdCard();
+    String getName();
+    int getLifePoint();
+    int getAttack();
+    int gatMana();
+    Optional<Effect> getEffect();
+    int getPlacementRounds();
+    void setAttack(int newValue);
+    void setLifePoint(int newValue);
+    void setName(String newName);
+    void setEffect(Optional<Effect> newEffect);
 }
