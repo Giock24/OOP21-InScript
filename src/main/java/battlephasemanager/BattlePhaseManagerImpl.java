72cee1287142835bbe78d3bd879e9fc64e7fc9e7
Giovanni Babbi <giovanni.babbi@studio.unibo.it>
2022-04-21 09:46:09 +0200

    - [ BattePhase ] - Fixed few warnings and Updated BattlePhaseManagerImpl

--- src/main/java/battlephasemanager/BattlePhaseManagerImpl.java
+++ src/main/java/battlephasemanager/BattlePhaseManagerImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import cards.ActivationEvent;
 import cards.Card;
+import cards.Effect;
 import shared.Player;
 
 public class BattlePhaseManagerImpl implements BattlePhaseManager {
@@ -13,8 +14,7 @@ public class BattlePhaseManagerImpl implements BattlePhaseManager {
     private final Player player;
     private final Player enemy;
 
-    private List<Optional<ActivationEvent>> effectPlayer = new ArrayList<>();
-    private List<Optional<ActivationEvent>> effectEnemy = new ArrayList<>();
+    private List<Optional<Effect>> effectPlayer = new ArrayList<>(), effectEnemy = new ArrayList<>();
     private final List<ActivationEvent> eventTarget = List.of(ActivationEvent.ONATTAKING, ActivationEvent.ONDEFENDING, ActivationEvent.ONDEATH);
     // private List<Optional<Card>> postBattleBoard = new ArrayList<>();
     
@@ -23,27 +23,29 @@ public class BattlePhaseManagerImpl implements BattlePhaseManager {
         this.enemy = p2;
     }
     
-    private List<Optional<ActivationEvent>> extractEvent(final Player target) {
-        final List<Optional<ActivationEvent>> tmp = new ArrayList<>();
+    private List<Optional<Effect>> extractEvent(final Player target) {
+        final List<Optional<Effect>> tmp = new ArrayList<>();
         // final aggiunto per sopprimere un warning, possibile problema;
         for(final Optional<Card> card : target.getCurrentBoard()) {
             if(card.isPresent()) {
                 if(card.get().getEffect().isPresent() && eventTarget.contains(card.get().getEffect().get().getActivationEvent())) {
-                    tmp.add(Optional.of(card.get().getEffect().get().getActivationEvent()));
+                    tmp.add(Optional.of(card.get().getEffect().get()));
                 }
                 else {
                     tmp.add(Optional.empty());
                 }
-            } 
+            }
+            else {
+                tmp.add(Optional.empty());
+            }
         }
         return tmp;
-    }
+    } 
             
     @Override
     public void handleEffect() {
         effectPlayer = extractEvent(player);
         effectEnemy = extractEvent(enemy);
-        
     }
     
     private List<Optional<Card>> handleBattle(final Player protagonist, final Player antagonist){
@@ -54,11 +56,13 @@ public class BattlePhaseManagerImpl implements BattlePhaseManager {
                     // Metodo dello sviluppo della battaglia.
                     // da aggiungere la parte per gestire gli effetti
                     
+                    //if(effectPlayer.get(i).isPresent()) {
+                    //    effectPlayer.get(i).get().useEffect(protagonist, antagonist, i);
+                    //}
                     
-                    
-                    
+                    // if(List.of(ActivationEvent.ONDEFENDING, ActivationEvent.ONDEATH).contains(antagonist))
                     // Se la carta è presente sottrae alla vita attuale di tale carta l'attacco della carta del protagonista
-                    // antagonist.getCurrentBoard().get(i).get().SetLife(antagonist.getCurrentBoard().get(i).get().getLife() - protagonist.getCurrentBoard().get(i).get().getAttack())
+                    antagonist.getCurrentBoard().get(i).get().setLifePoint(antagonist.getCurrentBoard().get(i).get().getLifePoint() - protagonist.getCurrentBoard().get(i).get().getAttack());
                     if(antagonist.getCurrentBoard().get(i).get().getLifePoint() <= 0) {
                         tmp.add(Optional.empty());
                     }
