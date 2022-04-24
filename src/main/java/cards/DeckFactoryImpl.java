cea3b55803b1764bacbd17b4df648f9aecfbf073
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-24 13:24:17 +0200

    - [Shared] - Add some cards in Deck Factory for testing

--- src/main/java/cards/DeckFactoryImpl.java
+++ src/main/java/cards/DeckFactoryImpl.java
@@ -9,13 +9,11 @@ public class DeckFactoryImpl implements DeckFactory {
 
     @Override
     public List<Card> getStandardDeck() {
-
-        
         
         for(int i=0; i==4; i++) {
             this.standardDeck.add(new CardFactoyImpl().noEffect("st-01", "Cane", 1, 1, 1));
         }
-        
+        this.standardDeck.add(new CardFactoyImpl().noEffect("st-04", "Sasso", 4, 0, 2));
         
         return this.standardDeck;
     }
