f22a1d28b0d5f8e1557057c4124477ef04fd1188
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-24 12:32:05 +0200

    - [Cards] -  resolve warning in DeckFactoryImpl

--- src/main/java/cards/DeckFactoryImpl.java
+++ src/main/java/cards/DeckFactoryImpl.java
@@ -8,7 +8,7 @@ public class DeckFactoryImpl implements DeckFactory {
     @Override
     public List<Card> getStandardDeck() {
 
-        List<Card> standardDeck = new ArrayList();
+        final List<Card> standardDeck = new ArrayList<>();
         
         //TODO use here the card factory and add all the cards
         
