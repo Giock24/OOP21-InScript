a02fd87682fa5b6cd5cc5e956b6594264b5194f6
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-04-25 11:00:19 +0200

    - [Cards] - Implement Draw Effect

--- src/main/java/effects/Draw.java
+++ src/main/java/effects/Draw.java
@@ -2,6 +2,8 @@ package effects;
 
 import cards.AbstractEffect;
 import cards.ActivationEvent;
+import cards.DeckFactoryImpl;
+import drawphasemanager.DrawPhaseManagerImpl;
 import shared.Player;
 
 public class Draw extends AbstractEffect{
@@ -20,7 +22,7 @@ public class Draw extends AbstractEffect{
     @Override
     public void useEffect(final Player cardOwner, final Player enemy, final int boardPosition) {
         
-        
+        new DrawPhaseManagerImpl(cardOwner, enemy).drawWithoutMana(cardOwner);
     }
 
 }
