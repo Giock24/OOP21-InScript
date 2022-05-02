829241a0ed3271c61d1ab58e601ce9ef420be515
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-05-02 11:05:17 +0200

    - [Shared] - Modified Elusive effect, Now can't defend but perform direct attack

--- src/main/java/effects/Elusive.java
+++ src/main/java/effects/Elusive.java
@@ -7,25 +7,27 @@ import shared.Player;
 public class Elusive extends AbstractEffect{
 
     public Elusive() {
-        super("Elusive", "Elude le carte avversarie a meno che non combatta con un altra carta Elusive");
+        super("Elusive", "Elude le carte avversarie, evitandone la difesa, a meno che non combatta con un altra carta Elusive");
         
     }
 
     @Override
     public ActivationEvent getActivationEvent() {
         
-        return ActivationEvent.ONDEFENDING;
+        return ActivationEvent.ONATTAKING;
     }
 
     @Override
     public void useEffect(final Player cardOwner, final Player enemy, final int boardPosition) {
         
-        if (!enemy.getCurrentBoard().get(boardPosition).get().getEffect().get().getNameEffect().equals(cardOwner.getCurrentBoard().get(boardPosition).get().getEffect().get().getNameEffect())) {
+        if (enemy.getCurrentBoard().get(boardPosition).isPresent() && !enemy.getCurrentBoard().get(boardPosition).get().getEffect().equals(cardOwner.getCurrentBoard().get(boardPosition).get().getEffect())) {
             
-            enemy.setLifePoints(enemy.getLifePoints() - cardOwner.getCurrentBoard().get(boardPosition).get().getAttack()); 
+            enemy.getCurrentBoard().get(boardPosition).get().setLifePoint(enemy.getCurrentBoard().get(boardPosition).get().getLifePoint()+cardOwner.getCurrentBoard().get(boardPosition).get().getAttack());
+            
+            enemy.setLifePoints(enemy.getLifePoints()-cardOwner.getCurrentBoard().get(boardPosition).get().getAttack());    
             
         }
-        // testare Poison attaccante contro elusive
+        
     }
 
 }
