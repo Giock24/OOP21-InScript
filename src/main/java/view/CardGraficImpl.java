d89a84dd0082a0f37289fc39195625f1c09aecb9
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-05-03 19:56:49 +0200

    - [Card-GUI] - Set Default immage for Card View

--- src/main/java/view/CardGraficImpl.java
+++ src/main/java/view/CardGraficImpl.java
@@ -3,7 +3,13 @@ package view;
 import java.util.Optional;
 
 import cards.Card;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class CardGraficImpl implements CardGrafic {
@@ -57,6 +63,9 @@ public class CardGraficImpl implements CardGrafic {
     @Override
     public VBox generateCardViewElement(final Optional<Card> selectedCardToShow) {
         final VBox cardViewElement = new VBox();
+        final BorderPane nameContainer = new BorderPane();
+        final Pane imageContainer = new Pane();
+        final BorderPane statsContainter = new BorderPane();
         
         if(selectedCardToShow.isPresent()) {
             
@@ -73,7 +82,46 @@ public class CardGraficImpl implements CardGrafic {
             final Label cardName = new Label();
             cardName.setText(card.getName());
             
-            cardViewElement.getChildren().add(cardName);
+            final Label cardMana = new Label();
+            cardMana.setText(" "+card.getMana());
+            
+            nameContainer.setLeft(cardName);
+            nameContainer.setRight(cardMana);
+            
+            final Label pipo = new Label();
+            
+            imageContainer.getChildren().add(pipo);
+            imageContainer.setStyle(" "
+                    + "-fx-background-image:url('standardDeckImage/Cane.png'); "
+                    + "-fx-background-repeat: no-repeat;\n"
+                    + "-fx-background-size: contain;\n"
+                    + "-fx-background-size: 100% 100%;");
+            imageContainer.setPadding(new Insets(100));
+            
+            final Label atkValue = new Label();
+            atkValue.setText(" "+card.getAttack());
+            atkValue.setPadding(new Insets(10));
+            
+            final Label effectDescription = new Label();
+            if (card.getEffect().isPresent()) {
+            effectDescription.setText(card.getEffect().get().getNameEffect()+": "+card.getEffect().get().getDescriptionEffect());
+            effectDescription.setWrapText(true);
+            }else {
+                effectDescription.setText("No Effect");
+            }
+            
+            final Label lifeValue = new Label();
+            lifeValue.setText(" "+card.getLifePoint());
+            lifeValue.setPadding(new Insets(10));
+            
+            statsContainter.setLeft(atkValue);
+            statsContainter.setCenter(effectDescription);
+            statsContainter.setRight(lifeValue);
+            
+            cardViewElement.getChildren().add(nameContainer);
+            cardViewElement.getChildren().add(imageContainer);
+            cardViewElement.getChildren().add(statsContainter);
+            
         } 
         
         return cardViewElement;
