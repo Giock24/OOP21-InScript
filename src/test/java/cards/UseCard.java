557f2399451b33103f507da0f71e53be78bf6c50
Michele Andrenacci <michele.andrenacci@studio.unibo.it>
2022-05-04 10:26:42 +0200

    - [Card-GUI] - Commented the text Class UseCard

--- src/test/java/cards/UseCard.java
+++ src/test/java/cards/UseCard.java
@@ -7,12 +7,12 @@ public class UseCard {
 	public static void main(String[] args) {
 			
 		
-		BaseCard dog = new BaseCard("st-00", "cane", 1, 1, 1, null);
+	/*	BaseCard dog = new BaseCard("st-00", "cane", 1, 1, 1, null);
 		BaseCard cat = new BaseCard("st-01", "gatto", 1, 0, 0, null);
 		
 		
 		assertEquals("st-00",dog.getIdCard());
 		assertEquals("st-01",cat.getIdCard());
-		
+	*/	
 	}
 }
