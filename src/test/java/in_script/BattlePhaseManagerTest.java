f846104f52567061e84ae8d730b36ba22a30ec46
Giovanni Babbi <giovanni.babbi@studio.unibo.it>
2022-06-23 10:20:18 +0200

    - [ TEST ] - Removed JSONParsingTest, Updated BattlePhaseManagerTest

--- src/test/java/in_script/BattlePhaseManagerTest.java
+++ src/test/java/in_script/BattlePhaseManagerTest.java
@@ -33,28 +33,9 @@ public class BattlePhaseManagerTest {
         IntStream.range(0, Player.NUM_CARD_BOARD).forEach( index -> {
             emptyBoard.add(Optional.empty());
         });
-        marco = new PlayerImpl(false, Collections.EMPTY_LIST, 0, 0, 0, marcosBoard, Collections.EMPTY_LIST );
-        aI = new PlayerImpl(true, Collections.EMPTY_LIST, 0, 0, 0, aIBoard, Collections.EMPTY_LIST );
+        marco = new PlayerImpl(false, Collections.emptyList(), 0, 0, 0, marcosBoard, Collections.emptyList() );
+        aI = new PlayerImpl(true, Collections.emptyList(), 0, 0, 0, aIBoard, Collections.emptyList() );
         battleManager = new BattlePhaseManagerImpl(marco, aI);
-    }
-    
-    @Test
-    void testAIEmptyBoard() {
-        marcosBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        marcosBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        marcosBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        marcosBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        marcosBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        
-        aIBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        aIBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        aIBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        aIBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        aIBoard.add(Optional.of(new CardFactoyImpl().noEffect( "Dog", 1, 1, 1, "standardDeckImage/Cane.png")));
-        
-        battleManager.startBattle(false);
-
-        assertEquals(emptyBoard, this.aI.getCurrentBoard());
     }
     
     @Test
@@ -97,42 +78,45 @@ public class BattlePhaseManagerTest {
     }
     
     @Test
-    void PoisonActivation() {
-        // impossibile testare lastwill effect, per qualche motivo le liste sono letteralmente identiche, ma vengono comunque riconosciute come diverse, anche se non è presente nessuna
-        // incoerenza, credo sia dovuto dal fatto che le carte create siano comunque considerate come carte diverse da quelle in cui cresce.
-        // Stesso discorso anche per putridume.
-        
-        // PER QUALCHE MOTIVO, FAR COMBATTERE UN LADRO E UN TEMPLARE RENDE COMPLETAMENTE IRRICONOSCIBILE LE CARTE CHE HANNO COMBATTUTO. LA CARTA è LETTERALMENTE LA STESSA
-        // MA VIENE COMUNQUE PRESA COME DIVERSA.
-        
-        // NON è POSSIBILE TESTARE NESSUN EFFETTO NE UNA NORMALE BATTAGLIA, IN QUANTO VENGONO TUTTI TRATTATI COME OGGETT DIVERSI PUR AVENDO LO STESSO STATO.
-        
-        // Tra l'altro, se un ladro combatte con un templare, questo si cura comunque del suo attacco.
+    void PlayerEffectActivation() {
         marcosBoard.add(Optional.of(new CardFactoyImpl().poisonEffect( "Viper", 1, 1, 2, "standardDeckImage/Vipera.png")));
         marcosBoard.add(Optional.of(new CardFactoyImpl().poisonEffect( "Viper", 1, 1, 2, "standardDeckImage/Vipera.png")));
-        marcosBoard.add(Optional.empty());
-        marcosBoard.add(Optional.empty());
-        marcosBoard.add(Optional.empty());
+        marcosBoard.add(Optional.of(new CardFactoyImpl().poisonEffect( "Viper", 1, 1, 2, "standardDeckImage/Vipera.png")));
+        marcosBoard.add(Optional.of(new CardFactoyImpl().poisonEffect( "Viper", 1, 1, 2, "standardDeckImage/Vipera.png")));
+        marcosBoard.add(Optional.of(new CardFactoyImpl().elusiveEffect( "Thief", 1, 3, 3, "standardDeckImage/Ladro.png")));
         
-        // Optional.of(new CardFactoyImpl().lastwillEffect("Herald of The Pit", 6, 6, 6, "deckOfThePitImage/Herald_Of_The_Pit.png", "Seo", 2, 1, Optional.of(new Armored()), "deckOfThePitImage/Seo.png"))
-        // Optional.of(new CardFactoyImpl().rottenEffect( "Mummy", 1, 4, 3, "standardDeckImage/Mummia.png"))
         aIBoard.add(Optional.of(new CardFactoyImpl().armoredEffect( "Templar", 2, 4, 5, "standardDeckImage/Templare.png")));
         aIBoard.add(Optional.empty());
-        aIBoard.add(Optional.empty());
-        aIBoard.add(Optional.empty());
-        aIBoard.add(Optional.empty());
+        aIBoard.add(Optional.of(new CardFactoyImpl().rottenEffect( "Mummy", 1, 4, 3, "standardDeckImage/Mummia.png")));
+        aIBoard.add(Optional.of(new CardFactoyImpl().lastwillEffect("Herald of The Pit", 6, 6, 6, "deckOfThePitImage/Herald_Of_The_Pit.png", "Seo", 2, 1, Optional.of(new Armored()), "deckOfThePitImage/Seo.png")));
+        aIBoard.add(Optional.of(new CardFactoyImpl().armoredEffect( "Templar", 2, 4, 5, "standardDeckImage/Templare.png")));
         
         resultBoard.add(Optional.empty());        
         resultBoard.add(Optional.empty());
-        resultBoard.add(Optional.empty());
-        resultBoard.add(Optional.empty());
-        resultBoard.add(Optional.empty());
+        resultBoard.add(Optional.of(new CardFactoyImpl().noEffect("Putridume", 3, 0, 3, "standardDeckImage/Putridume.png")));
+        resultBoard.add(Optional.of(new CardFactoyImpl().armoredEffect("Seo", 2, 1, 6, "deckOfThePitImage/Seo.png")));
+        resultBoard.add(Optional.of(new CardFactoyImpl().armoredEffect( "Templar", 3, 4, 5, "standardDeckImage/Templare.png")));
         
         battleManager.startBattle(false);
+
+        IntStream.range(0, Player.NUM_CARD_BOARD).forEach( index -> {
+            if(resultBoard.get(index).isPresent()) {
+                assertEquals(resultBoard.get(index).get().getName(), aIBoard.get(index).get().getName());
+                assertEquals(resultBoard.get(index).get().getLifePoint(), aIBoard.get(index).get().getLifePoint());
+                assertEquals(resultBoard.get(index).get().getAttack(), aIBoard.get(index).get().getAttack());
+                assertEquals(resultBoard.get(index).get().getMana(), aIBoard.get(index).get().getMana());
+                assertEquals(resultBoard.get(index).get().getImageURL(), aIBoard.get(index).get().getImageURL());
+                if(resultBoard.get(index).get().getEffect().isPresent()) {
+                    assertEquals(resultBoard.get(index).get().getEffect().get().getNameEffect(), aIBoard.get(index).get().getEffect().get().getNameEffect());
+                    assertEquals(resultBoard.get(index).get().getEffect().get().getDescriptionEffect(), aIBoard.get(index).get().getEffect().get().getDescriptionEffect());
+                    assertEquals(resultBoard.get(index).get().getEffect().get().getImageEffectURL(), aIBoard.get(index).get().getEffect().get().getImageEffectURL());
+                    assertEquals(resultBoard.get(index).get().getEffect().get().getActivationEvent(), aIBoard.get(index).get().getEffect().get().getActivationEvent());
+                }
+            }
+        });
         
-        assertEquals(resultBoard, this.aI.getCurrentBoard());
-        assertEquals(-1, this.aI.getLifePoints());
-        assertEquals(1, this.marco.getLifePoints());
+        assertEquals(-4, this.aI.getLifePoints());
+        assertEquals(4, this.marco.getLifePoints());
         
     }
     
